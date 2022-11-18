795da5dde792538f63526b0ec24ce37579240ada
Aniket_singh <singh7509811123@gmail.com>
2022-11-18 22:15:28 +0530

    initial commit

---
+++ SourceCode.py
@@ -0,0 +1,108 @@
+from distutils.cmd import Command
+from struct import pack
+from subprocess import CompletedProcess
+import cv2
+import csv
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import tkinter.messagebox
+
+
+# Giving a Function To The Button
+def TakeAttendance():
+    tkinter.messagebox.showinfo(
+        "Encoding Start.",  "please wait a few seconds")
+    path = 'images'
+    images = []
+    personNames = []
+    myList = os.listdir(path)
+    print(myList)
+
+    for cu_img in myList:
+        current_Img = cv2.imread(f'{path}/{cu_img}')
+        images.append(current_Img)
+        personNames.append(os.path.splitext(cu_img)[0])
+    print(personNames)
+
+
+# Face Encoding
+
+    def faceEncodings(images):
+        encodeList = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
+
+    def Attendance(name):
+        with open('Attendance.csv', 'r+') as f:
+            myDataList = f.readlines()
+            nameList = []
+            for line in myDataList:
+                entry = line.split(',')
+                nameList.append(entry[0])
+
+            if name not in nameList:
+                time_now = datetime.now()
+                tStr = time_now.strftime('%H:%M:%S')
+                dStr = time_now.strftime('%d/%m/%Y')
+                f.writelines(f'\n{name},{tStr},{dStr},{"Present"}')
+
+    encodeListKnown = faceEncodings(images)
+    print('All Encodings Complete!!!')
+    tkinter.messagebox.showinfo(
+        "Encoding Completed.",  "All Encodings Complete!!!")
+
+    cap = cv2.VideoCapture(0)
+
+    def new_func(faces):
+        faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+        return faces
+
+    while True:
+        ret, frame = cap.read()
+        faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+        faces = new_func(faces)
+
+        facesCurrentFrame = face_recognition.face_locations(faces)
+        encodesCurrentFrame = face_recognition.face_encodings(
+            faces, facesCurrentFrame)
+
+        for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+            matches = face_recognition.compare_faces(
+                encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(
+                encodeListKnown, encodeFace)
+
+            # print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = personNames[matchIndex].upper()
+
+                # print(name)
+
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y2 - 35), (x2, y2),
+                              (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, name, (x1 + 6, y2 - 6),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                Attendance(name)
+
+        cv2.imshow('Webcam', frame)
+        if cv2.waitKey(1) == 13:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    # Show Attendance
+
+
+def showAttendance():
+    os.startfile("Attendance.csv")
